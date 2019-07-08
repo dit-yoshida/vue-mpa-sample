3a4ae6ca8e38de3c1cef6e496fa31cf8b8d728a0
dit-hide <matsui.hidenori@ditgroup.jp>
2019-07-08 09:31:53 +0900

    add: webpackにbabelの設定を追加 (#2)
    * add: webpackにbabelの設定を追加
    * Fixed: コロン後ろスペース挿入、ダブルをシングルクォートに変更

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = function(api){
+  api.cache(true);
+
+  const presets = [
+    [
+      //プリセットに@babel/preset-env を指定する
+      '@babel/preset-env',
+      {
+        //サポートするブラウザ、この設定に応じて必要なpolyfillのみがimportされる
+        targets:{
+          ie: '11'
+        },
+        //必要なpolyfillのみをimportさせたい場合、'usage'を指定する
+        useBuiltIns: 'usage',
+        //polyfillを利用する core-js のバージョンを指定する(指定しないとバージョン２が利用され警告が出力される)
+        corejs: 3,
+        //Stage 4 未満のプロボーザルのpolyfillもimportされる
+        // corejs:{ version: 3, proposals: true },
+        //trueにすると利用しているポリフィルなどの情報が出力される
+        //polyfillがimportされているかどうかを確認するためのものなので必須ではない
+        debug: true
+      }
+    ]
+  ];
+  return{
+    presets
+  };
+};
